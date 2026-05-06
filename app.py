7218a6ab2bef046f97d5f83af86f8ba70e9462ad
Harshit999-ai <prajapatiharshit511@gmail.com>
2026-05-06 22:22:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+#This is simple code for running our app live using flask
+from flask import Flask,render_template,request
+from weather_fetcher import get_weather
 
+weather_api = "e5ee67b7cc420de7a779275e0ac8895e"
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST','GET'])
+def home():
+    weather = None
+    error = None
+    if request.method == 'POST':
+        city = request.form["city"]
+        weather = get_weather(city,weather_api)
+        return render_template("index.html",weather=weather,error=error)
+    else:
+        return "error"
+if __name__ == "__main__":
+    app.run(debug = True,port = 5000)
