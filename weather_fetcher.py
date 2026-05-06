b174902e6b99516a5fde22be77a49bc0baf69909
Harshit999-ai <prajapatiharshit511@gmail.com>
2026-05-06 22:24:32 +0530

    Update weather_fetcher.py

--- weather_fetcher.py
+++ weather_fetcher.py
@@ -1,1 +1,26 @@
+#this is our code to fetch weather info.
+import requests
 
+def get_weather(city,api_key):
+    API_KEY = api_key
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    res = requests.get(url)
+    data = res.json()
+
+    if data.get("cod") != 200:
+        return None, "City not found"
+
+    # Extract data
+    weather_data = {
+        "temp": data["main"]["temp"],
+        "feels_like": data["main"]["feels_like"],
+        "humidity": data["main"]["humidity"],
+        "wind_speed": data["wind"]["speed"],
+        "description": data["weather"][0]["description"],
+        "icon": data["weather"][0]["icon"],
+        "city": data["name"],
+        "country": data["sys"]["country"]
+    }
+
+    return weather_data, None
